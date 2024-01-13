182a29551fc2b8eb535b12c02325ddf8dd9ac44a
Tibor Korsos <tiborkorsos@gmail.com>
2024-01-13 20:39:03 +0100

    contact-js-kuldes

--- JS/main.js
+++ JS/main.js
@@ -8,6 +8,7 @@ const shopH2TextArray = ["Lorem ipsum, dolor sit amet consectetur adipisicing el
 const aboutH2TextArray = ["Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas consectetur ducimus laudantium sint cumque ipsam! Molestiae, odio quisquam doloribus sint maiores porro aperiam a excepturi iste esse fugit! Voluptatem assumenda totam commodi laudantium?", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae tempora minus nostrum.", "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta placeat aliquid provident error, quo nam ullam perspiciatis deserunt accusamus, adipisci rem quia necessitatibus rerum et.", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quod corporis soluta. Nihil fugit eius cumque vitae!"];
 const contactNameArray = ["name", "email", "subject", "message", "button"];
 const footerNameArray = ["Hibabejelentő", "Médiaajánlat", "Feltételek", "Adatvédelem", "Süti szabályzat", "Süti beállítások", "Gy.I.K."];
+const sendText = ["Név: ", "E-mail: ", "Tárgy: ", "Üzenet: "];
 
 const navDisplay = () => {
     let string = "";
@@ -65,29 +66,29 @@ const contactDisplay = () => {
     contactNameArray.forEach(name => {
         if(name === "message") {
             let message = "Üzenet";
-            string += `<div><label for="${name}">${message}</label><textarea name="${name}" id="${name}" class="input-area message-area"></textarea></div>`;
+            string += `<div><label for="${name}">${message}</label><textarea name="${name}" id="${name}" class="input-area message-area js-input-area"></textarea></div>`;
         }
         else if(name === "button") {
-            string += `<div><button type="submit" name="send_message" id="btn" class="contact-btn">Küldés</button></div>`;
+            string += `<div><button type="submit" name="send_message" id="js-btn-send" class="contact-btn">Küldés</button></div>`;
         }
         else if(name === "email") {
             let email = "E-mail";
-            string += `<div><label for="${name}">${email}</label><input type="email" name="${name}" id="${name}" class="input-area"></div>`;
+            string += `<div><label for="${name}">${email}</label><input type="email" name="${name}" id="${name}" class="input-area js-input-area"></div>`;
         }
         else {
             if(name === "name") {
                 let formName = "Név";
-                string += `<div><label for="${name}">${formName}</label><input type="text" name="${name}" id="${name}" class="input-area"></div>`;
+                string += `<div><label for="${name}">${formName}</label><input type="text" name="${name}" id="${name}" class="input-area js-input-area"></div>`;
             }
             if(name === "subject") {
                 let subject = "Tárgy";
-                string += `<div><label for="${name}">${subject}</label><input type="text" name="${name}" id="${name}" class="input-area"></div>`;
+                string += `<div><label for="${name}">${subject}</label><input type="text" name="${name}" id="${name}" class="input-area js-input-area"></div>`;
             }
         }
     });
     return `
     <article class="article-heading"><h2>Contact</h2></article>
-    <article class="contact-h2-text"><form class="contact-form-text">${string}</form></article>
+    <article class="contact-h2-text"><form class="contact-form-text js-contact-form-text" method="post">${string}</form></article>
     `;
 }
 
@@ -101,6 +102,18 @@ const footerDisplay = () => {
     return `<ul>${string}</ul><div class="copyright">Copyright © 2024 cégnév</div>`;
 }
 
+const handleSubmit = (e) => {
+    e.preventDefault();
+}
+
+const handleClick = (e) => {
+    let input = document.querySelectorAll('.js-input-area');
+    input.forEach( (elem, i) => {
+        // alert( sendText[i] + elem.value );
+        console.log( sendText[i] + elem.value );
+    })
+}
+
 const render = () => {
     nav.innerHTML = navDisplay();
     container.innerHTML = containerDisplay();
@@ -109,6 +122,8 @@ const render = () => {
     document.querySelector('.js-about').innerHTML = aboutDisplay();
     document.querySelector('.js-contact').innerHTML = contactDisplay();
     footer.innerHTML = footerDisplay();
+    document.querySelector('.js-contact-form-text').addEventListener("submit", handleSubmit);
+    document.querySelector('#js-btn-send').addEventListener("click", handleClick);
 }
 
 render();
